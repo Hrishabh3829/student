8a4bbf9e2a600047604394fd6a9b2884c3819ce2
Hrishabh3829 <2200032748@kluniversity.in>
2024-12-07 10:38:17 +0530

    New Updates

---
+++ src/components/StudentCard/StudentProfiles.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./StudentProfiles.css";
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+
+const StudentProfiles = ({ students }) => {
+    return (
+        <div className="student-cards-container">
+            {students.map((student) => (
+                <div key={student.id} className="student-card">
+                    <div className="profile-photo">
+                        {/*<img src={student.profileImage} alt={`${student.name}'s profile`} />*/}
+                        <InsertEmoticonIcon/>
+                    </div>
+                    <div className="student-info">
+                        <h3>{student.name}</h3>
+                        <p><strong>ID:</strong> {student.id}</p>
+                        <p><strong>Total Marks:</strong> {student.studentmarks}</p>
+                        <p><strong>Course:</strong> {student.studentcourse}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default StudentProfiles;
